bb15f6f1837ca8515c12db1f40f40ec852329c6b
tommya98 <tommya98@naver.com>
2021-10-30 17:31:05 +0900

    feat: Add basic html and getDate function

---
+++ js/time.js
@@ -0,0 +1,34 @@
+const date = document.querySelector("date");
+const clock = document.querySelector("clock");
+
+function getDate() {
+  const today = new Date();
+  const todayYear = today.getFullYear();
+  const todayMonth = String(today.getMonth() + 1).padStart(2, "0");
+  const todayDate = String(today.getDate()).padStart(2, "0");
+  const todayDayNum = today.getDay();
+
+  console.log(
+    `${todayYear}년 ${todayMonth}월 ${todayDate}일(${getTodayDay(todayDayNum)})`
+  );
+}
+
+function getTodayDay(num) {
+  if (num === 0) {
+    return "일";
+  } else if (num === 1) {
+    return "월";
+  } else if (num === 2) {
+    return "화";
+  } else if (num === 3) {
+    return "수";
+  } else if (num === 4) {
+    return "목";
+  } else if (num === 5) {
+    return "금";
+  } else {
+    return "토";
+  }
+}
+
+getDate();
