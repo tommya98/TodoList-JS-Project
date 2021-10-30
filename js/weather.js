6a37f9615332dce1fcfc9baaeba6054038f78b10
tommya98 <tommya98@naver.com>
2021-10-30 23:40:40 +0900

    Add weather.js and json package

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector(".temperature");
+const city = document.querySelector(".location");
+const API_KEY = "b42110a47749c0360e6df8a5f299c39f";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
