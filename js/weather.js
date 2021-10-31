96846e97a3d6732cf17052da85c923ad4293684a
tommya98 <tommya98@naver.com>
2021-10-31 16:37:43 +0900

    Fix weather lan and position

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,19 @@
 const weather = document.querySelector(".temperature");
+const description = document.querySelector(".description");
 const city = document.querySelector(".location");
 const API_KEY = "b42110a47749c0360e6df8a5f299c39f";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+  console.log(lat, lon);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+      description.innerText = `${data.weather[0].description}`;
+      weather.innerText = `${data.main.temp}℃`;
     });
 }
 
